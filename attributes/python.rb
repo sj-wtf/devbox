0e71936164128d704a55fd3721a440091870257d
steve <steve@johnson.pizza>
2016-12-11 05:17:59 -0600

    reorganize directories

---
+++ attributes/python.rb
@@ -0,0 +1,24 @@
+default['devbox']['python']['packages'] = [
+  'python2-pip',
+#   'python34',
+#   'python34-pip'
+]
+
+default['devbox']['python']['pip_packages'] = [
+  'arrow',
+  'flake8',
+  'matplotlib',
+  'records',
+  'requests',
+  'numpy',
+  'virtualenv'
+]
+
+default['devbox']['python']['apm_packages'] = [
+  'linter-flake8'
+]
+
+default['devbox']['python']['versions'] = [
+  '2.7',
+#   '3.4'
+]
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             'devbox'
+maintainer       'Steve Johnson'
+maintainer_email 'steve [at] johnson [doot] pizza'
+description      'An environment to dev in'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
